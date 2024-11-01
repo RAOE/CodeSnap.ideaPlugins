0cbb704c5c79f6db06e1afc7095e9d7bf58931e4
xuyuanfeng <986771570@qq.com>
2024-11-02 01:05:15 +0800

    feat(blog):  add commitlint-config-wizardoc

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        // 自定义规则
-        'body-leading-blank': [1, 'always'], // 确保body开始有一个空行
-        'footer-leading-blank': [1, 'always'], // 确保footer开始有一个空行
-        // 其他规则...
-    }
+    extends: ['wizardoc']
 }
