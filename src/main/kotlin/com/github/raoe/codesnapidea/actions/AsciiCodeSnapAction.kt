5a491a484afe69de34640ba1ce0706af004315fa
xuyuanfeng <986771570@qq.com>
2024-11-10 16:14:13 +0800

    [Feat] Code Snap Drop-down tab option feature development

---
+++ src/main/kotlin/com/github/raoe/codesnapidea/actions/AsciiCodeSnapAction.kt
@@ -0,0 +1,39 @@
+package com.github.raoe.codesnapidea.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import javax.swing.Icon
+
+/**
+ *  ClassName：AsciiCodeSnapAction
+Package:com.github.raoe.codesnapidea.actions
+@DATE:10/11/2024 4:02 pm
+@Author:XuYuanFeng
+TODO: ASCII CodeSnap
+ */
+class AsciiCodeSnapAction: AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        val editor: Editor? = event.getData(CommonDataKeys.EDITOR)
+        val project: Project? = event.getData(CommonDataKeys.PROJECT)
+        val selectedText: String? = editor?.selectionModel?.selectedText
+        val message = StringBuilder()
+        if (!selectedText.isNullOrEmpty()) {
+            message.append(selectedText).append(" Selected!JPG")
+            // call the native function
+        } else {
+            message.append("No text selected!")
+        }
+        val title = "Selection Info"
+        val icon: Icon = Messages.getInformationIcon()
+        Messages.showMessageDialog(
+            project,
+            message.toString(),
+            title,
+            icon
+        )
+    }
+}
