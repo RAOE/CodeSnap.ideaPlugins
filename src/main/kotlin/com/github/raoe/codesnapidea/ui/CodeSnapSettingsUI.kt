7499b72ace84f5d6476244858a8317978dfbd8c5
xuyuanfeng <986771570@qq.com>
2024-11-17 19:48:50 +0800

    [feature_dev_0.1] UI design

--- src/main/kotlin/com/github/raoe/codesnapidea/ui/CodeSnapSettingsUI.kt
+++ src/main/kotlin/com/github/raoe/codesnapidea/ui/CodeSnapSettingsUI.kt
@@ -1,77 +1,93 @@
-package com.github.raoe.codesnapidea.ui;
-
 import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.diagnostic.thisLogger
-import org.jdesktop.swingx.JXTextField
+import java.awt.Dimension
 import java.awt.FlowLayout
 import java.io.File
-import java.util.*
 import javax.swing.*
 
-/**
- * ClassName：CodeSnapSettingsUI
- * Package:com.github.raoe.codesnapidea.ui
- *
- * @DATE:13/11/2024 9:38 pm
- * @Author:XuYuanFeng TODO:
- */
+
 class CodeSnapSettingsUI {
     private val properties: PropertiesComponent = ServiceManager.getService(PropertiesComponent::class.java)
     private var defaultSavePath: String
         get() = properties.getValue("codeSnap.defaultSavePath", "/default/path")
         set(value) = properties.setValue("codeSnap.defaultSavePath", value)
-    private val myPanel: JPanel = JPanel(FlowLayout(FlowLayout.LEFT))
-    private val mySavePathField: JTextField = JTextField(20)
-    private val mySavePathButton: JButton = JButton("Browse...")
-    private val mySavePathLabel: JLabel = JLabel("默认保存路径:")
-    init {
-        // 使用 defaultSavePath 属性来设置 JTextField 的值
-        defaultSavePath = "/default/path"
-        myPanel.add(mySavePathLabel)
-        myPanel.add(mySavePathField)
-        myPanel.add(mySavePathButton)
+    private val defaultPanel: JPanel = JPanel().apply {
+        layout = BoxLayout(this, BoxLayout.Y_AXIS)
+        border = BorderFactory.createEmptyBorder(0, 10, 0, 10)
+    }
+    private val basicSettingTitle: JLabel = JLabel("Project settings")
+    private val defaultSavePathLabel: JLabel = JLabel("Default image save path:")
+    private val mySavePathArea: JTextField = JTextField(20)  // 5行40列
+    private val mySavePathButton: JButton = JButton("Choose")
+    private val defaultJLabel: JLabel = JLabel("Default image format:")
+    private val defaultJComboBox: JComboBox<String> = JComboBox(arrayOf("jpg", "png", "ascii", "jpeg", "svg"))
+    private val defaultJsonConfiguration: JLabel = JLabel("Default JSON configuration:")
+    private val optionConfig: JLabel = JLabel("Option configuration")
+    //1、默认剪贴板快捷键：
+    //              2、默认快照快捷键:
+    //              3、默认快照格式设置
+    //              4、config 自定义配置支持外部导入
+    //              5、默认生成的背景颜色选择
+    //              6、水印名称配置
+    //              7、命令行能力？
+    private val listData:List<String> = listOf("Default clipboard shortcuts","Default snapshot shortcut","Default snapshot format settings","Watermark name configuration","Command-line capabilities")
+    init{
+        val firstRowPanel = JPanel(FlowLayout(FlowLayout.LEFT,5,5)).apply {
+            add(defaultSavePathLabel)
+            add(mySavePathArea)
+            add(mySavePathButton)
+        }
+        val secondRowPanel = JPanel(FlowLayout(FlowLayout.LEFT,5,5)).apply {
+            add(defaultJLabel)
+            add(defaultJComboBox)
+        }
+        val thirdRowPanel = JPanel(FlowLayout(FlowLayout.LEFT,5,5)).apply {
+            add(defaultJsonConfiguration)
+        }
+        val fourthRowPanel = JPanel(FlowLayout(FlowLayout.LEFT,5,5)).apply {
+            add(optionConfig)
+            for (ele in listData) {
+                 val box:JCheckBox = JCheckBox(ele);
+                 add(box)
+            }
+        }
+        defaultPanel.add(firstRowPanel)
+        defaultPanel.add(secondRowPanel)
+        defaultPanel.add(thirdRowPanel)
+        defaultPanel.add(fourthRowPanel)
         mySavePathButton.addActionListener {
             val fileChooser = JFileChooser()
-            fileChooser.selectedFile = File(mySavePathField.text)
-            if (fileChooser.showOpenDialog(myPanel) == JFileChooser.APPROVE_OPTION) {
-                mySavePathField.text = fileChooser.selectedFile.absolutePath
-                // 更新 defaultSavePath 属性
+            fileChooser.selectedFile = File(mySavePathArea.text.trim())
+            if (fileChooser.showOpenDialog(defaultPanel) == JFileChooser.APPROVE_OPTION) {
+                mySavePathArea.text = fileChooser.selectedFile.absolutePath
                 defaultSavePath = fileChooser.selectedFile.absolutePath
             }
         }
     }
 
     fun getPanel(): JPanel {
-        return myPanel
+        return defaultPanel
     }
 
     fun isModified(): Boolean {
-        // 检查是否修改了设置
-        return mySavePathField.text != defaultSavePath
+        return mySavePathArea.text.trim() != defaultSavePath
     }
 
     fun apply() {
-        // 应用设置的逻辑
-        // 这里需要具体实现
-        defaultSavePath = mySavePathField.text
-        thisLogger().info("app apply>>>>>>>>>>>>>>>>>>>>>>>")
+        defaultSavePath = mySavePathArea.text.trim()
+        // 日志记录
     }
 
     fun reset() {
-        // 重置设置的逻辑
-        // 这里需要具体实现
-        mySavePathField.text = defaultSavePath
-        thisLogger().info("app reset>>>>>>>>>>>>>>>>>>>>>>>")
+        mySavePathArea.text = defaultSavePath
+        // 日志记录
     }
 
     fun saveSettings() {
-        val properties = ServiceManager.getService(PropertiesComponent::class.java)
-        properties.setValue("codeSnapSavePath", mySavePathField.text)
+        properties.setValue("codeSnapSavePath", mySavePathArea.text.trim())
     }
 
     fun loadSettings() {
-        val properties = ServiceManager.getService(PropertiesComponent::class.java)
-        mySavePathField.text = properties.getValue("codeSnapSavePath", "defaultSavePath")
+        mySavePathArea.text = properties.getValue("codeSnapSavePath", defaultSavePath)
     }
 }
